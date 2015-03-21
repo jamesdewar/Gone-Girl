a1ef0d3afb8145e6f8147b09c00b4834de913a10
jamesdewar <james.dewar@ividata.com>
2015-03-21 11:30:41 +0000

    Initial Commit

---
+++ Marco PoloTests/Marco_PoloTests.swift
@@ -0,0 +1,36 @@
+//
+//  Marco_PoloTests.swift
+//  Marco PoloTests
+//
+//  Created by James Dewar on 21/03/15.
+//  Copyright (c) 2015 IviData. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Marco_PoloTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
